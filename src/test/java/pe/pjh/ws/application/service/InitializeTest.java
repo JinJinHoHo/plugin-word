ed54ce5f4df5eecaa9f7fefb12bafc8754c348a8
박진호 <parkjh@isung.com>
2024-01-19 17:10:56 +0900

    - 초기화 서비스 개발 중. (코멘트 추가.)

--- src/test/java/pe/pjh/ws/application/service/InitializeTest.java
+++ src/test/java/pe/pjh/ws/application/service/InitializeTest.java
@@ -11,22 +11,32 @@ import java.util.List;
 
 class InitializeTest {
 
+    /**
+     * 초기화 플로우 테스트
+     */
     @Test
     public void initializeTest() {
+
+        //테스트 디렉토리
         String workingDirectory = System.getProperty("user.dir");
         String testPath = workingDirectory + "/.test";
+
+        //기존 테스트 디렉토리 삭제.
         File file = new File(testPath);
         if (file.exists()) file.delete();
 
+        //테스트를 위한 설정 값.
         SettingService settingService = new SettingService(
                 testPath,
                 List.of(new DataSetSetting())
         );
-
         DataSetManager dataSetManager = new DataSetManager();
 
+        //초기화 처리.
         Initialize initialize = new Initialize(settingService, dataSetManager);
 
+        //Todo 추가 테스트 로직 정의 필요.
         assert file.exists();
     }
+
 }
